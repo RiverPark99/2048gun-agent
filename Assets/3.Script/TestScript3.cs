f1edab9cd62118ea00ee16af68c8378632c5c431
RiverPark99 <phs201007@gmail.com>
2026-01-29 09:56:08 +0900

    ml_agnet, 학원컴 스크립트 테스트

---
+++ Assets/3.Script/TestScript3.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class TestScript3 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("TestScript3: Hello from Claude at school!");
+        Debug.Log("TestScript3: Filesystem MCP is working perfectly!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
